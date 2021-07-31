eeecef194e703d9fa51f868dfd0c60fbc67aca4f
Bilyana Andonova <60629112+BAndonova@users.noreply.github.com>
2021-07-31 21:42:42 +0200

    server.js update

--- server.js
+++ server.js
@@ -1,16 +1,10 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const routes = require("./routes");
 
 const app = express();
+const port = process.env.PORT || "1337";
+app.set("port", port);
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use('/', routes);
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(port, () => console.log(`Server running on localhost:${port}`));
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiURL: 'www.BBlog.com/api'
+  apiURL: 'https://bb-bblog.herokuapp.com/'
 };
